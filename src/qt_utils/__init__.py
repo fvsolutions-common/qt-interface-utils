b55889bf6f8e3ec9af30c65055cac2eb3f79e5c9
floris vernieuwe <floris@vernieuwe.eu>
2023-12-06 13:58:22 +0100

    Adde licence and about dialog

--- src/qt_utils/__init__.py
+++ src/qt_utils/__init__.py
@@ -1,4 +1,6 @@
 from . import messaging
 from .messaging import catch_exception
+from .about_dialog import QAboutDialog
+from .license_dialog import LicenseDialog
 
-__all__ = ["messaging", "catch_exception"]
+__all__ = ["messaging", "catch_exception", "QAboutDialog", "LicenseDialog"]
---
+++ src/qt_utils/about_dialog.py
@@ -0,0 +1,34 @@
+import sys
+
+from PySide2.QtCore import QObject
+from PySide2.QtWidgets import QMessageBox
+
+from .messaging import catch_exception
+
+
+class QAboutDialog(QObject):
+    def __init__(self, parent, appcontext):
+        super().__init__()
+        self.appcontext = appcontext
+        self._parent = parent
+
+    @catch_exception("Failed to show about dialog")
+    def show(self):
+        """Build and show the about window"""
+        version = self.appcontext.build_settings["full_version"]
+        author = self.appcontext.build_settings["author"]
+        environment = self.appcontext.build_settings["environment"]
+        copyright = self.appcontext.build_settings["copyright"]
+        app_name = self.appcontext.build_settings["app_name"]
+
+        text = f"""<center>
+                    <h1>{app_name}</h1>
+                    </center>
+                    <p>Version: {version}<br/>
+                    Author: {author}<br/>
+                    Enviroment: {environment}<br/>
+                    Copyright &copy; {copyright}<br/>
+                    Python: {sys.version}</p>
+                    """
+
+        QMessageBox.about(self._parent, "About", text)
