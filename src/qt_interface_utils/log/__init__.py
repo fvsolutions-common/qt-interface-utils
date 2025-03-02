a9c93370bcb4ef28034d8871d12f0c887d1c49ed
Floris Vernieuwe <floris@vernieuwe.eu>
2025-03-02 15:45:15 +0100

    cleanup

--- src/qt_interface_utils/log/__init__.py
+++ src/qt_interface_utils/log/__init__.py
@@ -2,4 +2,4 @@ from .dockable_logger import QDockableLoggingWidget
 from .file_logger import FileLogger
 from .stdout_logging import setup_standard_log
 
-__all__ = ["QDockableLoggingWidget", "FileLogger", "setup_standard_log", "SentryLogger"]
+__all__ = ["QDockableLoggingWidget", "FileLogger", "setup_standard_log"]
